0829797ccb33036ef016e1c383914d7f8fb07ef0
Davi Suga <daviciencia1@gmail.com>
2020-10-29 14:32:23 -0300

    refactor(org screena, app.js): removed unused code

--- App.js
+++ App.js
@@ -1,16 +1,3 @@
 import App from "./src";
-import Reactotron, {
-  networking,
-  openInEditor,
-  trackGlobalErrors,
-  asyncStorage,
-} from "reactotron-react-native";
-Reactotron.configure({ host: "192.168.15.25" })
-  .useReactNative()
-  .use(asyncStorage())
-  .use(networking())
-  .use(openInEditor())
-  .use(trackGlobalErrors())
-  .connect();
 
 export default App;
